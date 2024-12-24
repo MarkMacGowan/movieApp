740cbd944867811d07772ff605224c2e2fa957df
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2024-12-24 11:28:28 +0000

    Update movie_form.rb

--- movieApp/app/models/movie_form.rb
+++ movieApp/app/models/movie_form.rb
@@ -5,7 +5,7 @@ class MovieForm < ApplicationRecord
   validates :year, presence:true
   validates :cast, length: {in: 4..50}
   validates :description, presence: true
-  validates :description, length: {maximum:50}
+  validates :description, length: {maximum:100}
   validates :dvd, acceptance: true
   
 end
